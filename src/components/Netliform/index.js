40b99b97cb14b7caf3a1ad9da0cb02c18bb2309a
Paul Barron <p.barron@outlook.com>
2021-03-23 00:56:16 +0000

    Add pk-add-form-latest.

---
+++ src/components/Netliform/index.js
@@ -0,0 +1,29 @@
+const Netliform = () => {
+    return (
+        <div className={styles.netliformContainer}>
+            <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Phone: <input type="tel" name="phone" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Employee</option>
+      <option value="follower">Employer</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+        </div>
+    );
+}
---
+++ src/pages/services-copy.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+//Components
+import Netliform from "components/Netliform"
+
+export default () => {
+  return <Netliform />
+}
